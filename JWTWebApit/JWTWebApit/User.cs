11d3abacfac576a8955c72587263ef5e5ee25f29
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-08-30 22:38:20 +0500

    JWT first

---
+++ JWTWebApit/JWTWebApit/User.cs
@@ -0,0 +1,13 @@
+﻿namespace JWTWebApit
+{
+    public class User
+    {
+        internal object RefreshToken;
+
+        public string Username { get; set; } = string.Empty;
+
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
